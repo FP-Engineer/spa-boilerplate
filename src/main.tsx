c4e73eec44ac396dc04109460d1313bfeba3ef3f
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-09-12 22:14:00 +0200

    fix: fixed node module import statement

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import '@radix-ui/themes/styles.css';
-import 'https://unpkg.com/open-props';
-import 'https://unpkg.com/open-props/normalize.min.css';
+import 'open-props';
+import 'open-props/normalize.min.css';
 import { Theme } from '@radix-ui/themes';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
