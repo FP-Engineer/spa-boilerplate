15d293295be3ef7617168b22f3c39aadba189ce9
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-09-13 23:38:47 +0200

    chore: added network layer mock request handlers

--- src/__mocks/handlers.ts
+++ src/__mocks/handlers.ts
@@ -1,6 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { DefaultBodyType, MockedRequest, RestHandler } from 'msw';
+import {
+	DefaultBodyType, MockedRequest, RestHandler, rest,
+} from 'msw';
 
 export const handlers = [
-
+	rest.get('https://api.example.com/happy/path', (_, res, ctx) => res(
+		ctx.status(200),
+		ctx.json({
+			payload: 'success',
+		}),
+	)),
+	rest.get('https://api.example.com/server/error', (_, res, ctx) => res(
+		ctx.status(501),
+		ctx.json({ message: 'server error' }),
+	)),
 ] as RestHandler<MockedRequest<DefaultBodyType>>[];
