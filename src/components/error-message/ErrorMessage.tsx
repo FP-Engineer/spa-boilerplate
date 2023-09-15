02372d5c0a97b150dd434c88dbe46d6d41401a6b
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-09-15 22:04:15 +0200

    chore: removed unused style sheet

--- src/components/error-message/ErrorMessage.tsx
+++ src/components/error-message/ErrorMessage.tsx
@@ -2,8 +2,6 @@ import { faBug } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Callout } from '@radix-ui/themes';
 
-import { container } from './ErrorMessage.css';
-
 export interface Props {
 	children: string;
 }
@@ -12,7 +10,6 @@ export function ErrorMessage({ children }: Props) {
 
 	return (
 		<Callout.Root
-			className={ container }
 			variant='surface'
 			color='red'
 		>
