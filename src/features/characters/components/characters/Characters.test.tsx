94d47f8d8d47ef99e07089162488c6bdf3240e4e
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-10-13 22:22:00 +0200

    chore: added characters test suite

---
+++ src/features/characters/components/characters/Characters.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+
+import { Characters } from './Characters';
+
+describe('Characters Test Suite', () => {
+
+	// TODO: inject data source to make characters component testable
+	it.skip('renders without crashing.', () => {
+
+		render(
+			<Characters />
+		);
+
+	});
+
+});
