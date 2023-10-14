ec8308558aa1612ef2d2e23f0df9ec37695d4cdd
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-10-14 23:13:07 +0200

    fix: changed avatar background color

--- src/features/characters/components/character/Character.css.ts
+++ src/features/characters/components/character/Character.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const avatarContainer = style({
 	blockSize: '100%',
-	backgroundColor: 'color-mix(in hsl longer hue, hsl(120deg 18.73% 88.15%) 20%, Canvas)',
+	backgroundColor: 'color-mix(in hsl longer hue, hsl(120deg 20% 60%) 20%, Canvas)',
 });
 
 export const fullHeight = style({
