f750c9e79ac80858abe1880a441e801218823102
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-10-14 22:43:01 +0200

    fix: defined avatar background color as relative system color

--- src/features/characters/components/character/Character.css.ts
+++ src/features/characters/components/character/Character.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const avatarContainer = style({
 	blockSize: '100%',
-	backgroundColor: '#24292F',
+	backgroundColor: 'color-mix(in hsl longer hue, hsl(120deg 18.73% 88.15%) 20%, Canvas)',
 });
 
 export const fullHeight = style({
